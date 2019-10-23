ed6bdcb8f170978daa469afa500ae99415a2b508
Marko <todic.marko@outlook.com>
2019-10-23 16:53:07 +0200

    Added interface call

--- WebApiGit/WebApiGit/Controllers/PredmetController.cs
+++ WebApiGit/WebApiGit/Controllers/PredmetController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using WebApiGit.Models;
+using WebApiGit.Repository.IRepository;
 
 namespace WebApiGit.Controllers
 {
     public class PredmetController : ApiController
     {
+
     }
 }
