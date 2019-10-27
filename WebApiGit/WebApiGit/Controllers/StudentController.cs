d88f88bfb8da765ef48d1d72734ed6264f9cb5db
Marko <todic.marko@outlook.com>
2019-10-27 12:33:07 +0100

    Created Student Get method

--- WebApiGit/WebApiGit/Controllers/StudentController.cs
+++ WebApiGit/WebApiGit/Controllers/StudentController.cs
@@ -11,5 +11,23 @@ namespace WebApiGit.Controllers
 {
     public class StudentController : ApiController
     {
+        private readonly IStudentRepository _studentRepository;
+        public StudentController(IStudentRepository studentRepository)
+        {
+            this._studentRepository = studentRepository;
+        }
+
+        //GET api/student
+        [HttpGet]
+        [Route("api/student")]
+        public IHttpActionResult Get()
+        {
+            List<StudentModel> studentList = _studentRepository.Get();
+            if (studentList.Count == 0)
+            {
+                return NotFound();
+            }
+            else return Ok(studentList);
+        }
     }
 }
