71484a4baf1e8c7f5d48e1182e3f449721c5afa9
Marko <todic.marko@outlook.com>
2019-10-27 12:21:37 +0100

    Student Interface completed

--- WebApiGit/WebApiGit/Repository/IRepository/IStudentRepository.cs
+++ WebApiGit/WebApiGit/Repository/IRepository/IStudentRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApiGit.Models;
 
 namespace WebApiGit.Repository.IRepository
 {
     public interface IStudentRepository
     {
+        List<StudentModel> Get();
+        void Create(StudentModel studentModel);
+        void Edit(students studentInDb, StudentModel studentModel);
+        void Delete(students studentInDB);
+        students GetById(int id);
+        StudentModel GetStudentDTO(students studentInDb);
     }
 }
