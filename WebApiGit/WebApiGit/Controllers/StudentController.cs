301f0dcfc03472d1a93d0f8091f728b81c441ab1
Marko <todic.marko@outlook.com>
2019-10-27 12:30:16 +0100

    Created Student Controller

---
+++ WebApiGit/WebApiGit/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiGit.Models;
+using WebApiGit.Repository.IRepository;
+
+namespace WebApiGit.Controllers
+{
+    public class StudentController : ApiController
+    {
+    }
+}
