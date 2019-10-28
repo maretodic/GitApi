d153e062f2ac2990b1aa82766a7cf0a97ab6aa03
Marko <todic.marko@outlook.com>
2019-10-28 17:12:21 +0100

    Completed Profesor Repository Interface

--- WebApiGit/WebApiGit/Repository/IRepository/IProfesorRepository.cs
+++ WebApiGit/WebApiGit/Repository/IRepository/IProfesorRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApiGit.Models;
 
 namespace WebApiGit.Repository.IRepository
 {
     public interface IProfesorRepository
     {
+        List<ProfesorModel> Get();
+        profesors GetById(int id);
+        ProfesorModel GetProfesorDTO(profesors profesorInDb);
+        void Create(ProfesorModel profesorDTO);
+        void Edit(profesors profesorInDb, ProfesorModel profesorDTO);
+        void Delete(profesors profesorInDb);
     }
 }
